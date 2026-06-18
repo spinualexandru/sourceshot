a85dbb7b389bfb3aa2d5a9bede05b98233ad9903
Alex Spinu <spinualexandru@outlook.com>
2026-06-18 15:30:13 +0300

    refactor: add lit decorators

--- apps/website/src/main.ts
+++ apps/website/src/main.ts
@@ -1,10 +1,10 @@
 import { html, render } from "lit";
-import "./components/source-shot-app.ts";
 import "./style.css";
 
 const appElement = document.querySelector<HTMLDivElement>("#app")!;
 const { renderSnapshotFromHash } = await import("./code.ts");
 
 if (!(await renderSnapshotFromHash(appElement))) {
+  await import("./components/source-shot-app.ts");
   render(html`<source-shot-app></source-shot-app>`, appElement);
 }
